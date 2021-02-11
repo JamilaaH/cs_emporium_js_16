4bdc542814428b6bffb5b394b1fec55b90cfb303
Jamila <jaja_mimi@hotmail.com>
2021-02-11 14:10:28 +0100

    carrousel responsive et navbar rectif

--- public/js/main.js
+++ public/js/main.js
@@ -5,10 +5,14 @@ let logo = document.querySelector('.logo')
 let legend = document.querySelectorAll('.text')
 let grandTitre = document.querySelectorAll('.title')
 let navBar = document.querySelector('.navbar')
+let nav = document.querySelector('.navbar-collapse')
+console.log(nav);
 let header = document.querySelector('header')
 // console.log(legend);
 // console.log(btnNoir);
 // console.log(navBar);
+let allBtn = document.querySelectorAll(".boutonCarou");
+
 
 // mode sombre
 let sombre = () => {
@@ -18,8 +22,14 @@ let sombre = () => {
     elementNav.forEach(element => {
         element.style.color = "white"
     });
+    allBtn.forEach(element => {
+        element.classList.replace('btn-outline-dark', 'btn-outline-light')
+    });
     legend.forEach(element => {
         element.style.color = "white"
+    });
+    allBtn.forEach(element => {
+        element.classList.replace('btn-outline-dark', 'btn-outline-light')
     });
     grandTitre.forEach(element => {
         element.style.color = "white"
@@ -41,6 +51,10 @@ let blanc = () => {
     legend.forEach(element => {
         element.style.color = "black"    
     });
+    allBtn.forEach(element => {
+        element.classList.replace('btn-outline-light', 'btn-outline-dark')
+    });
+
     grandTitre.forEach(element => {
         element.style.color = "black"    
     });
@@ -54,11 +68,14 @@ btnBlanc.addEventListener('click', blanc)
 window.addEventListener('scroll', ()=> {
     header.style = "height:70px"
     navBar.classList.replace('flex-column', 'flex-row')
-    navBar.style = "position:fixed ; top:0; left:0 ; right:0 ; z-index:99"
+    navBar.style = "position:fixed ; top:0; left:0 ; right:0 ; z-index:99;"
+    nav.style ="margin-left:10%"
 
     if (window.scrollY == 0) {
         navBar.classList.replace('flex-row', 'flex-column')
         navBar.style.backgroundColor = null 
+        nav.style ="margin-left:null"
+
     }
 })
 
@@ -74,7 +91,7 @@ let btnSeCo = document.querySelector('.seConnecter')
 
 let popUp = document.querySelector('#connexion')
 let btnClose = document.querySelector('.fermer')
-console.log(btnSubs);
+// console.log(btnSubs);
 
 
 //bouton connexion du navbar
@@ -87,11 +104,6 @@ btnClose.addEventListener('click', ()=> {
     popUp.style = 'display: none'
 })
 
-
-// btn connexion du popup
-// btnCo.addEventListener('click', ()=> {
-//     connexion.classList.remove('d-none')
-// })
 
 
 btnCo.addEventListener('click', ()=> {
@@ -108,3 +120,50 @@ btnSubs.addEventListener('click', ()=> {
     connexion.classList.add('d-none')
 })
 
+
+
+// carrousel 
+
+let btnUn = document.querySelector('.un')
+let btnDeux = document.querySelector('.deux')
+let btnTrois = document.querySelector('.trois')
+let btnQuatre = document.querySelector('.quatre')
+let test = document.querySelector('.test')
+
+
+btnDeux.addEventListener('click', ()=> {
+    test.style = "transform:translateX(-200px); transition:2s"    
+})
+
+btnTrois.addEventListener('click', ()=> {
+    test.style = "transform:translateX(-400px); transition:2s"    
+})
+btnQuatre.addEventListener('click', ()=> {
+    test.style = "transform:translateX(-600px); transition:2s"
+    
+})
+btnUn.addEventListener('click', ()=> {
+    test.style = "transform:translateX(0%); transition:2s"
+    
+})
+
+
+console.log(allBtn);
+
+// let myEvent = (e) => {
+//     var btn = e.target
+//     if (btn.classList.contains('deux')) {
+//         test.style = "transform:translateX(-40%); transition:2s"
+//     } else if (btn.classList.contains('trois')) {
+//         test.style = "transform:translateX(-40%); transition:2s" 
+        
+//     } else if (btn.classList.contains('quatre')) {
+//         test.style = "transform:translateX(-40%); transition:2s"
+//     } else if (btn.classList.contains('un')) {
+//         test.style = "transform:translateX(-40%); transition:2s"
+//     }
+// }
+// allBtn.addEventListener('click', myEvent)
+
+// console.log(test.clientWidth);
+// console.log(test.offsetWidth);
